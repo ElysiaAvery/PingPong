4e796551263b8fa2c7fb2214c14cfd72f092f132
Elysia Nason <elysia.avery@gmail.com>
2016-08-19 11:29:57 -0700

    add a list of numbers that print

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,7 @@
 //Business Logic
-function pingPong(number) {
-  var multiple = [1234567890];
+function PingPong(number) {
   if (number % 15 === 0) {
-    return ("Ping-Pong" + );
+    return "Ping-Pong";
   }
   if (number % 3 === 0) {
     return "Ping";
@@ -15,18 +14,46 @@ function pingPong(number) {
   }
   return number.toString();
 }
-
-
 for (let i = 1; i <= 1000; i++) {
-  console.log(pingPong(i));
+  console.log(PingPong(i));
 }
+function Range(end) {
+  var myArray = [];
+  for (var i = 0; i <= end; i += 1) {
+    myArray.push(i);
+  }
+  alert(myArray);
+  return myArray;
+};
+// console.log(Range(end));{}
+
+// function PingPong(number) {
+//   if (number % 15 === 0) {
+//     return "Ping-Pong";
+//   }
+//   if (number % 3 === 0) {
+//     return "Ping";
+//   }
+//   if (number % 5 === 0) {
+//     return "Pong";
+//   }
+//   if (number % 2 === 0) {
+//     return "Star Trek!"
+//   }
+//   return number.toString();
+// }
+// for (let i = 1; i <= 1000; i++) {
+//   console.log(PingPong(i));
+// }
 
 //Front End Logic
 $(document).ready(function(){
   $("form").submit(function(event){
     var userInput = $("input#number").val();
-    var output = pingPong(userInput);
+    var output = PingPong(userInput);
+    var numberRange = Range(userInput);
     $("#result").append("<li>"+output+"</li>");
+    $("#result").append("<li>"+numberRange+"</li>");
   event.preventDefault();
   });
 });
